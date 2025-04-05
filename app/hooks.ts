e3ddcf3ccc0adba0669bf9256cb13c96625979d9
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-05 16:29:08 +0100

    Custom ts hook

---
+++ app/hooks.ts
@@ -0,0 +1,10 @@
+import {
+  UseSelector,
+  useSelector,
+  TypedUseSelectorHook,
+  useDispatch,
+} from "react-redux";
+import { RootState, AppDispatch } from "./store";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch<AppDispatch>();
